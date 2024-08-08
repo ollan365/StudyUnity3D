9eecf6fb2d959dd142af18daba9c44211c33a1d2
ollan365 <80192257+ollan365@users.noreply.github.com>
2024-08-08 18:00:48 +0900

    Paticle
    공격, 피격 파티클 추가

--- Assets/2. Scripts/Effect/ParticleManager.cs
+++ Assets/2. Scripts/Effect/ParticleManager.cs
@@ -9,6 +9,7 @@ public class ParticleManager : MonoBehaviour
     [SerializeField] private GameObject[] particlePrefabs;
     [SerializeField] private GameObject[] testObjectForParticle;
     [SerializeField] private Particle testParticle;
+    [SerializeField] private float projectileSpeed;
     private void Awake()
     {
         if (Instance == null)
@@ -26,7 +27,7 @@ public class ParticleManager : MonoBehaviour
         foreach (GameObject obj in testObjectForParticle)
             PlayParticle(obj, testParticle);
     }
-    public void PlayParticle(GameObject obj, Particle particle)
+    public GameObject PlayParticle(GameObject obj, Particle particle)
     {
         GameObject particleObj = null;
 
@@ -37,16 +38,72 @@ public class ParticleManager : MonoBehaviour
         {
             case Particle.Enemy_Summon:
             case Particle.Friend_Summon:
+            case Particle.Attack_PlayerTeam_BySword:
+            case Particle.PlayerTeam_Sttaff_Charging:
+            case Particle.Attack_Enemy_BySword:
+            case Particle.Enemy_Sttaff_Charging:
                 particleObj.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
                 break;
             case Particle.Heal:
                 particleObj.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
                 break;
+            case Particle.Enemy_INVINCIBILITY:
+            case Particle.PlayerTeam_INVINCIBILITY:
+                break;
+
+            case Particle.PlayerTeam_Sttaff_Projectile:
+            case Particle.Enemy_Sttaff_Projectile:
+                particleObj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                break;
         }
 
         particleObj.GetComponent<ParticleSystem>().Play();
+        return particleObj;
     }
+    public float AttackParticle(Transform src, Transform dst)
+    {
+        bool isEnemyAttack = src.GetComponent<Object>().Type == ObjectType.ENEMY;
+        if (src.GetComponent<Object>().AttackType == WeaponType.SWORD)
+        {
+            StartCoroutine(AttackSwordCoroutine(dst, isEnemyAttack));
+        }
+        else if (src.GetComponent<Object>().AttackType == WeaponType.STAFF)
+        {
+            StartCoroutine(AttackStaffCoroutine(src, dst, isEnemyAttack));
+            return 2;
+        }
+        return Time.deltaTime;
+    }
+    private IEnumerator AttackSwordCoroutine(Transform dst, bool isEnemy)
+    {
+        yield return new WaitForSeconds(0.2f);
 
+        if (!isEnemy) PlayParticle(dst.gameObject, Particle.Attack_PlayerTeam_BySword);
+        else PlayParticle(dst.gameObject, Particle.Attack_Enemy_BySword);
+    }
+    private IEnumerator AttackStaffCoroutine(Transform src, Transform dst, bool isEnemy)
+    {
+        GameObject chargingObject = null, projectile = null;
+
+        // 차징 시작
+        if (isEnemy) chargingObject = PlayParticle(src.gameObject, Particle.Enemy_Sttaff_Charging);
+        else chargingObject = PlayParticle(src.gameObject, Particle.PlayerTeam_Sttaff_Charging);
+        yield return new WaitForSeconds(0.5f);
+
+        // 투사체 생성
+        if (isEnemy) projectile = PlayParticle(src.gameObject, Particle.Enemy_Sttaff_Projectile);
+        else projectile = PlayParticle(src.gameObject, Particle.PlayerTeam_Sttaff_Projectile);
+        yield return new WaitForSeconds(0.5f);
+
+        // 차징 끝 -> 투사체 날림
+        Destroy(chargingObject);
+
+        Vector3 direction = (dst.transform.position - src.transform.position).normalized;
+        projectile.GetComponent<Rigidbody>().velocity = direction * projectileSpeed;
+        yield return new WaitForSeconds(0.5f);
+
+        Destroy(projectile);
+    }
     private int Int(Particle text)
     {
         switch (text)
